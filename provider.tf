cfcd5f4a7a5f18f24b9ee2daf1ef45c6a27f0d1c
ojas <ojaslanjekar603@gmail.com>
2026-09-08 13:35:27 +0530

    Configure Terraform S3 remote state

--- provider.tf
+++ provider.tf
@@ -1,6 +1,12 @@
 terraform {
   required_version = ">= 1.6.0"
 
+  backend "s3" {
+    bucket = "terraform-aws-nginx-ha-ojaslan-state"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
